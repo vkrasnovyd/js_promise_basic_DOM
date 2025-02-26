a076254a44fd5141757c144df558f0ff2748217e
vkrasnovyd <v.krasnovyd@gmail.com>
2025-02-26 17:13:10 +0100

    Create first promises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+
+// Promises
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => resolve());
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('No actions within 3 seconds')), 3000);
+});
+
+// Callbacks handling
+promise1
+  .then((successCallback) => successHandler(successCallback))
+  .catch((errorCallback) => errorHandler(errorCallback));
+
+promise2
+  .then((successCallback) => successHandler())
+  .catch((errorCallback) => errorHandler());
+
+function successHandler() {
+  addHandlerMessage('Promise was resolved!');
+}
+
+function errorHandler() {
+  addHandlerMessage('Promise was rejected!', ['error-message']);
+}
+
+function addHandlerMessage(messageText, customClasses = []) {
+  const message = document.createElement('div');
+
+  message.classList.add('message', ...customClasses);
+  message.textContent = messageText;
+  body.appendChild(message);
+}
